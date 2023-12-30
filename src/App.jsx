e4ac073cf5148751a889a18e7863e09cded06bc4
Daniel Ramon Campos <drc89.sxm@gmail.com>
2023-12-30 13:40:35 +0100

    scroll-snap

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,24 @@ import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <>
-      <div>
         <Header />
-        <HeroSection/>
-      </div>
+      <main className="snap-y snap-mandatory relative w-full h-screen overflow-auto overflow-x-hidden">
+        <div className="snap-center">
+          <HeroSection/>
+        </div>
+        <div className="snap-center">
+          <HeroSection/>
+        </div>
+        <div className="snap-center">
+          <HeroSection/>
+        </div>
+        <div className="snap-center">
+          <HeroSection/>
+        </div>
+        <div className="snap-center">
+          <HeroSection/>
+        </div>
+      </main>
     </>
   );
 }
