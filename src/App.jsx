6de945adab5f19f0c08f543508484b99e227c3d8
Daniel Ramon Campos <drc89.sxm@gmail.com>
2023-12-30 12:29:17 +0100

    header + font family Gotham

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-red-500 text-3xl">Hello Tesla</h1>
+        <Header />
       </div>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import Logo from "../assets/Logo";
+
+function Header() {
+    return (
+    <main className="py-3 px-10 flex items-center fixed top-0 w-full justify-between">
+        <div className="flex flex-grow basis-0">
+            <Logo />
+        </div>
+
+        <nav>
+            <ul className="flex text-sm [&>li>a]:text-gray-900 [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2">
+                <li><a href="#"></a>Model S</li>
+                <li><a href="#"></a>Model 3</li>
+                <li><a href="#"></a>Model X</li>
+                <li><a href="#"></a>Model Y</li>
+                <li><a href="#"></a>Powerwall</li>
+                <li><a href="#"></a>Carga</li>
+            </ul>
+        </nav>
+
+        <nav className="flex flex-grow justify-end basis-0">
+            <ul className="flex text-sm [&>li>a]:text-gray-900 [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2">
+                <li><a href="#"></a>Soporte</li>
+                <li><a href="#"></a>Tienda</li>
+                <li><a href="#"></a>Cuenta</li>
+                <li><a href="#"></a>Menú</li>
+            </ul>
+        </nav>
+    </main>
+    );
+}
+
+export default Header;
